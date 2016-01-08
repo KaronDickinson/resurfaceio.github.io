c382433b30517e9d70976a3ba58f6c6dae0f7d8d
Rob Dickinson <robfromboulder@gmail.com>
2016-01-08 11:51:32 -0700

    #8 Add social media buttons/images

--- js/application.js
+++ js/application.js
@@ -1,2 +1,2 @@
-var pnotify_stack = {"dir1": "up", "dir2": "left", "firstpos1": 15, "firstpos2": 15};
+var pnotify_stack = {"dir1": "up", "dir2": "left", "firstpos1": 8, "firstpos2": 8};
 PNotify.prototype.options.styling = "bootstrap3";
